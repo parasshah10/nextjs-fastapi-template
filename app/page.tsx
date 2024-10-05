6bb9d139323e9f8477da12b802644ce815bcae95
Paras <shahparas4@gmail.com>
2024-10-05 18:20:54 +0530

    switch to inter font

--- app/page.tsx
+++ app/page.tsx
@@ -47,7 +47,7 @@ export default function Home() {
   };
 
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+    <div className="flex flex-col items-center justify-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
       <main className="flex flex-col items-center gap-8 w-full max-w-2xl">
         <Image
           className="dark:invert"
@@ -57,7 +57,7 @@ export default function Home() {
           height={38}
           priority
         />
-        <ol className="list-inside list-decimal text-sm text-center font-[family-name:var(--font-geist-mono)]">
+        <ol className="list-inside list-decimal text-sm text-center">
           <li className="mb-2">
             Get started by editing{" "}
             <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
